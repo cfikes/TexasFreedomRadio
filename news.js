33b94cff24d2c0ba6547132c78642f7defac89d3
Christopher Fikes <cfikes@fikesmedia.com>
2014-05-25 21:26:38 -0500

    Working on Nexus

--- news.js
+++ news.js
@@ -16,9 +16,7 @@ function loadnews() {
 	
     function xmlParser(data) {
     var xml = data;
-    $('#nload').fadeOut();
-    $("#nlist").html("");
-	var counter = 0;
+
     $(xml).find("item").each(function () {
 
         var ntitle = $(this).find("title").text();
@@ -26,6 +24,7 @@ function loadnews() {
         ndate = ndate.substring(0, ndate.length - 15);
         var ndesc = $(this).find("description").text();
         var nlink = $(this).find("link").text();
+	
 		//$("#nlist").append('<li><h3 id="ntitle">' + ntitle + '</h3><p>' + ndesc + '</p><p>' + ndate + '<br>');			
 		$("#nlist").append('<li><a href="' + nlink + '" target="_system"><h2>' + ntitle + '</h2><p><strong>' + ndate +'</strong></p></a>');
 		$('#nlist').listview('refresh'); 
